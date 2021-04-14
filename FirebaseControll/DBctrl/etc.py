64234d1e2d12a09272da8394225697e9740a2e0e
ATRON3273 <adgjl16430@gmail.com>
2021-04-15 03:16:09 +0900

    Add, modify userinfo setting functions

--- FirebaseControll/DBctrl/etc.py
+++ FirebaseControll/DBctrl/etc.py
@@ -3,12 +3,31 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 from pprint import pprint
+from hashlib import sha256
 
 if not firebase_admin._apps:
     cred = credentials.Certificate("./key/key.json")
     firebase_admin.initialize_app(cred,{'databaseURL' : 'https://decisive-sylph-308301-default-rtdb.firebaseio.com/'})
 
 # 기타 조작 함수
+
+def make_uid(login_id):
+    """
+    id 문자열을 이용해 uid 값을 생성하는 함수
+    uid 생성에 sha256 함수를 사용
+
+    login_id(str) : uid 값을 생성할 아이디 문자열
+    """
+    hashed_id = sha256(login_id.encode()).hexdigest()
+    # hash값을 int 최대값으로 나눈 나머지 값을 사용
+    uid = str(int(hashed_id, 16) % 2147483647)
+
+    return uid
+
+def hash_password(password):
+    # 보안 이유로 사용하면 안 될 함수
+    return sha256(password.encode()).hexdigest()
+    
 
 def get_all_data():
     """
@@ -17,6 +36,8 @@ def get_all_data():
     print("Are you sure? (if yes, input 'y' or 'Y'.)")
     response = input()
 
-    if response is 'y' or 'Y' or '':
+    if response is 'y' or 'Y':
         dir = db.reference()
         return dir.get()
+    else:
+        return
--- FirebaseControll/DBctrl/userinfo.py
+++ FirebaseControll/DBctrl/userinfo.py
@@ -2,15 +2,27 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+from . import etc
+from .profile import get_profile
+
 from pprint import pprint
 
 if not firebase_admin._apps:
     cred = credentials.Certificate("./key/key.json")
     firebase_admin.initialize_app(cred,{'databaseURL' : 'https://decisive-sylph-308301-default-rtdb.firebaseio.com/'})
 
+# USERINFO 데이터베이스 구조
 """
-def make_user(login_id, login_pw, email):
-    dir = db.reference('USERINFO')
+USERINFO':
+{
+    'darak':
+    {
+        'OAuth': '~~~~',
+        'auth_email': '~~~~@~~~~.~~~',
+        'login_pw': '~~~',
+        'uid': '~~~'
+    }
+}
 """
 
 def login(login_id, password):
@@ -23,10 +35,16 @@ def login(login_id, password):
     """
     pwstring = db.reference('USERINFO/' + login_id + '/login_pw').get()
 
-    if pwstring == password:
+    if pwstring == etc.hash_password(password):
         return True
     else:
         return False
+
+def get_all_userinfo():
+    """
+    DB에 있는 모든 계정의 정보를 불러오는 함수
+    """
+    return db.reference('USERINFO/').get()
 
 def get_userinfo(login_id):
     """
@@ -36,7 +54,68 @@ def get_userinfo(login_id):
     """
     return db.reference('USERINFO/' + login_id).get()
 
-def update_email(login_id, email):
+def make_userinfo(login_id, login_pw, email, OAuth):
+    """
+    DB에 새로운 유저 로그인 정보를 생성(회원가입)
+    생성에 성공하면 True, 실패하면 False를 반환
+
+    login_id(str) : 유저의 로그인 아이디
+    login_pw(str) : 유저의 로그인 비밀번호
+    email(str) : 유저의 인증 이메일주소
+    OAuth(str) : 계정 OAuth 연결 정보
+    """
+    # 현재 DB상에 해당 아이디의 사용자가 없으면 진행
+    if get_userinfo(str(login_id)) is None:
+        # uid 값 생성
+        tmp_id = login_id
+
+        # 생성한 uid가 현재 사용하지 않는 uid값이 나올 때까지 반복
+        while True:
+            tmp_id = tmp_id + '0'
+            uid = etc.make_uid(str(tmp_id))
+
+            if get_profile(uid) is None:
+                break
+        
+        # password 해시화
+        hash_pw = etc.hash_password(login_pw)
+
+        # DB에 생성
+        dir = db.reference('USERINFO').child(str(login_id))
+        dir.set({
+            'OAuth': OAuth,
+            'auth_email': str(email),
+            'login_pw': hash_pw,
+            'uid': uid
+        })
+        print("Producing " + login_id + " account success.")
+        return True
+    # 현재 DB상에 해당 아이디의 사용자가 있으면 중단  
+    else:
+        print("Already exist ID value.")
+        return False
+
+def delete_userinfo(login_id):
+    """
+    유저의 계정 정보를 삭제
+    삭제를 성공하면 True, 아니면 False를 반환
+
+    login_id(str) : 유저의 로그인 아이디
+    """
+    # 현재 DB상에 해당 아이디의 사용자가 있으면 진행
+    if get_userinfo(str(login_id)) is not None:
+        # DB에서 삭제
+        dir = db.reference('USERINFO').child(str(login_id))
+        dir.delete()
+
+        print("Delete " + login_id + " account.")
+        return True
+    # 현재 DB상에 해당 아이디의 사용자가 없으면 중단  
+    else:
+        print("There's no ID in DB.")
+        return False
+
+def change_email(login_id, email):
     """
     유저 계정의 이메일 주소를 수정하는 함수
 
@@ -46,12 +125,23 @@ def update_email(login_id, email):
     dir = db.reference('USERINFO/' + login_id)
     dir.update({'auth_email':email})
 
-def change_pw(login_id, new_pw):
+def change_pw(login_id, check_pw, new_pw):
     """
     유저 계정의 비밀번호를 수정하는 함수
 
     login_id(str) : 유저의 로그인 아이디
+    check_pw(str) : 수정할 비밀번호
     new_pw(str) : 수정할 비밀번호
     """
-    dir = db.reference('USERINFO/' + login_id)
-    dir.update({'login_pw':new_pw})
+    cur_user = get_userinfo(login_id)
+    exist_pw = cur_user['login_pw']
+
+    # 기존의 비밀번호가 맞는지 확인 후 변경
+    if exist_pw == etc.hash_password(check_pw):
+        dir = db.reference('USERINFO/' + login_id)
+        dir.update({'login_pw':new_pw})
+        print("Password is changed successfully.")
+        return True
+    else:
+        print("Password is not matched.")
+        return False
